f27bdc628ce3a98f85d4e23a7ca792fff7de448d
Rohtirsr1408 <rohitrana8041@gmail.com>
2025-08-23 20:48:59 +0530

    Course Schedule -1

---
+++ Graphs/Topological Sorting/Course Schedule -1.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+         vector<int>indegree(numCourses,0);
+         vector<vector<int>>adj(numCourses);
+         for(int i=0;i<prerequisites.size();i++){
+               int u=prerequisites[i][1];
+            int v=prerequisites[i][0];
+            adj[u].push_back(v);
+         }
+         for(int i=0;i<prerequisites.size();i++){
+            int u=prerequisites[i][1];
+            int v=prerequisites[i][0];
+            indegree[v]++;
+         } 
+         queue<int>q;
+         for(int i=0;i<numCourses;i++){
+            if(!indegree[i]) q.push(i);
+         }
+         vector<int>topo;
+         while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+      
+
+            for(auto adjnode:adj[node]){
+              
+                indegree[adjnode]--;
+                if(!indegree[adjnode])q.push(adjnode);
+            }
+         }
+         
+         if(topo.size()==numCourses)return true;
+         return false;
+    }
+};
