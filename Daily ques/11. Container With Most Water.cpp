5cb3555dfdff8efa3bbab8014c64f1f2d14145d3
Rohtirsr1408 <rohitrana8041@gmail.com>
2025-10-04 14:26:10 +0530

    Binary search

---
+++ Daily ques/11. Container With Most Water.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+    int l=0;
+    int r=height.size()-1;
+    int area=0;
+    while(l<r){
+        if(height[l]<height[r]){
+           area=max(area,height[l]*(r-l));
+           l++;
+        }
+        else{
+            area=max(area,height[r]*(r-l));
+            r--;
+        }
+    } 
+    return area;     
+
+    }
+};
